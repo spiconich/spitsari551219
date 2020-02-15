6fbc87019b3d411e0a922b5e40a7ac6a9d9cd109
Nikolai <megoze@ro.ru>
2020-02-15 19:04:56 +0500

    загружен проект на github
    testss - main exe, assist - dll
    на данный момент решение находит и отображает все присутсвующие на компьютере физические носители и логические тома

---
+++ assist/dllmain.cpp
@@ -0,0 +1,131 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <Windows.h>
+#include <string>
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+extern "C" __declspec(dllexport) void CheckSystem(HWND mainhWnd)
+{
+  //  MessageBox(mainhWnd, "dll func ready to work v2", "pobeda_6", NULL);
+
+  //  InvalidateRect(mainhWnd, NULL, TRUE); // помечает окно как устаревшее,требует его перересовки (WM_PAINT)
+}
+
+extern "C" __declspec(dllexport) void CheckPhysDrives(HWND mainhWnd)
+{    
+     std::string fileName = "\\\\.\\PhysicalDrive";
+     int foundDriveCount = 0;
+     int msgDriveX = 50;
+     int msgDriveY = 50;
+
+     for (int DriveNum = 0; DriveNum < 30; DriveNum++)
+     {
+         std::string fullPath = fileName + std::to_string(DriveNum);
+         HANDLE fileHandle = CreateFileA(
+             fullPath.c_str(),
+             GENERIC_READ,
+             FILE_SHARE_READ | FILE_SHARE_WRITE,
+             NULL,
+             OPEN_EXISTING,
+             FILE_ATTRIBUTE_NORMAL,
+             NULL);
+         if (fileHandle != INVALID_HANDLE_VALUE)
+         {
+             foundDriveCount++;
+         }
+     }
+         if (foundDriveCount == 0)
+         {
+             MessageBox(mainhWnd, "No drives found on this PC", "error_8", NULL);
+         }
+         else
+         {
+             HDC hdc = (HDC)GetDC(mainhWnd);
+             ::TextOut(hdc, msgDriveX, msgDriveY, "Found Drives list:", 19);
+             for (int DriveNum = 0; DriveNum < 30; DriveNum++)
+             {
+                 std::string fullPath = fileName + std::to_string(DriveNum);
+                 HANDLE fileHandle = CreateFileA(
+                     fullPath.c_str(),
+                     GENERIC_READ,
+                     FILE_SHARE_READ | FILE_SHARE_WRITE,
+                     NULL,
+                     OPEN_EXISTING,
+                     FILE_ATTRIBUTE_NORMAL,
+                     NULL);
+
+                 if (fileHandle != INVALID_HANDLE_VALUE)
+                 {
+                     msgDriveY = msgDriveY + 20;
+                     ::TextOut(hdc, msgDriveX, msgDriveY, fullPath.c_str(), 19);
+                 }
+             }
+         }
+    
+
+}
+
+extern "C" __declspec(dllexport) void CheckVolume(HWND mainhWnd)
+{
+    std::string fileName = "\\\\.\\";
+    std::string letters[26] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+    const int msgVolumeX = 200;
+    int msgVolumeY = 50;
+    int foundVolumeCount = 0;
+    for (int DriveNum = 0; DriveNum < 26; DriveNum++)
+    {   std::string fullPath = fileName + letters[DriveNum]+":";
+        HANDLE fileHandle = CreateFileA(
+            fullPath.c_str(),
+            GENERIC_READ,
+            FILE_SHARE_READ | FILE_SHARE_WRITE,
+            NULL,
+            OPEN_EXISTING,
+            FILE_ATTRIBUTE_NORMAL,
+            NULL);
+        if (fileHandle != INVALID_HANDLE_VALUE)
+        {
+            foundVolumeCount++;
+        };
+    }
+    if (foundVolumeCount == 0)
+      {
+         MessageBox(mainhWnd, "No volumes found on this PC", "error_6", NULL);
+      }
+    else
+      {
+        HDC hdc = (HDC)GetDC(mainhWnd);
+        ::TextOut(hdc, msgVolumeX, msgVolumeY, "Found Volumes list:", 20);
+        for (int DriveNum = 0; DriveNum < 26; DriveNum++)
+          {  
+            std::string fullPath = fileName + letters[DriveNum] + ":";
+            HANDLE fileHandle = CreateFileA(
+                fullPath.c_str(),
+                GENERIC_READ,
+                FILE_SHARE_READ | FILE_SHARE_WRITE,
+                NULL,
+                OPEN_EXISTING,
+                FILE_ATTRIBUTE_NORMAL,
+                NULL);
+            if (fileHandle != INVALID_HANDLE_VALUE)
+              {
+                msgVolumeY = msgVolumeY + 20;
+                ::TextOut(hdc, msgVolumeX, msgVolumeY, letters[DriveNum].c_str(), 1);
+              }
+           
+          }
+      }
+}
---
+++ testss/testss.cpp
@@ -0,0 +1,178 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+const char g_szClassName[] = "chckrClass";
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    //std::string VolumeNum[] = { "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
+   // int DriveNum[31] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+    HDC hDeviceContext;
+    PAINTSTRUCT paintStruct;
+    RECT rectPlace;
+    HFONT hFont;
+    switch (msg)
+    {
+    case WM_COMMAND:
+
+        switch (LOWORD(wParam))
+        {
+        case 1:
+        {
+            typedef UINT(*MYFUNC)(UINT, UINT);
+            // Указатель на функцию
+            void (*dllCheckVolume) (HWND);
+            void (*dllCheckPhysDrives) (HWND);
+            // Загружаем динамически подключаемую библиотеку
+            HMODULE hLib = LoadLibrary("assist.dll");
+            if (hLib == NULL)
+            {
+                MessageBox(hWnd, "Error while loading DLL", "Error_1", NULL);
+                break;
+            }
+
+            // Настраиваем адрес функции
+            dllCheckVolume = (void (*)(HWND))GetProcAddress(hLib, "CheckVolume");
+            if (!dllCheckVolume)
+            {
+                MessageBox(hWnd, "Error while getting func address", "Error_2", NULL);
+                return GetLastError();
+            }
+            dllCheckPhysDrives = (void (*)(HWND))GetProcAddress(hLib, "CheckPhysDrives");
+            if (!dllCheckPhysDrives)
+            {
+                MessageBox(hWnd, "Error while getting func address", "Error_7", NULL);
+                return GetLastError();
+            }
+            // Вызываем функцию из библиотеки
+            dllCheckPhysDrives(hWnd);
+            dllCheckVolume(hWnd);
+
+            // Отключаем библиотеку
+            if (!FreeLibrary(hLib))
+            {
+                MessageBox(hWnd, "Error while dropping lib from memory", "Error_3", NULL);
+                return GetLastError();
+            }
+
+        }
+        break;
+
+        case 2:
+            DestroyWindow(hWnd);
+            break;
+        }
+
+
+        break;
+    //case WM_PAINT:
+   // {   if (wm_paint_id == 1)
+     //   {
+      //     hDeviceContext = BeginPaint(hWnd, &paintStruct);
+       //    GetClientRect(hWnd, &rectPlace);
+       //    SetTextColor(hDeviceContext, NULL);
+        //   hFont = CreateFont(30, 0, 0, 0, 0, 0, 0, 0,
+       //    DEFAULT_CHARSET,
+        //   0, 0, 0, 0,
+        //   "Arial Bold" );
+        //   SelectObject(hDeviceContext, hFont);
+         //  DrawText(hDeviceContext, "TEXT", 6, &rectPlace, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+        //   EndPaint(hWnd, &paintStruct);
+        // };
+   // break;
+   // }
+    
+    case WM_CLOSE:
+        DestroyWindow(hWnd);
+        break;
+   
+    case WM_DESTROY:
+    {
+        PostQuitMessage(NULL);
+        break;
+    }
+    default:
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{  
+    WNDCLASSEX wc;
+    HWND hwnd;
+    HWND hwndButtonExit;
+    HWND hwndButtonOk;
+    MSG Msg;
+
+    
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = g_szClassName;
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+ 
+    if (!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, "Class not register", "Error_4", MB_ICONINFORMATION | MB_OK);
+        return 0;
+    }
+    hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        g_szClassName,
+        "CHECKER",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        NULL, NULL,
+        hInstance,
+        NULL);
+    if (hwnd == NULL)
+    {
+        MessageBox(NULL, " Window Creation Failed", "Error_5 ", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+    hwndButtonExit = CreateWindow(
+        "BUTTON", 
+        "OK", 
+        WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // styles
+        500, 
+        525, 
+        100, 
+        30, 
+        hwnd, // parent window
+        (HMENU)1, // ВОТ ЭТО ЗНАЧЕНИЕ
+        (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE),
+        NULL); // pointer not needed
+    hwndButtonOk = CreateWindow(
+        "BUTTON", 
+        "EXIT", 
+        WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 
+        600,
+        525, 
+        100, 
+        30, 
+        hwnd, // parent window
+        (HMENU)2, // ВОТ ЭТО ЗНАЧЕНИЕ
+        (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE),
+        NULL); // pointer not needed
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+    while (GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+    return Msg.wParam;
+}
+
+
